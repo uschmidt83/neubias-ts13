faf08829515d2641c652a716bdc9e8431d3b3dac
Uwe Schmidt <mail@uweschmidt.org>
2019-10-14 23:18:39 +0100

    bump

--- scikit-image/my_workflow.py
+++ scikit-image/my_workflow.py
@@ -17,17 +17,16 @@ from skimage.measure import regionprops
 from scipy.ndimage import label
 
 
-description = """
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-"""
-
-
 def parse_args():
+    description = """
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    """
+
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=description)
 
     data = parser.add_argument_group("input")
@@ -39,7 +38,7 @@ def parse_args():
     model.add_argument('--threshold-method', type=str,   required=False, default='otsu', help="automatic thresholding method", choices=['isodata','li','mean','minimum','otsu','triangle','yen'])
 
     output = parser.add_argument_group("output")
-    output.add_argument('--output-file', type=str, required=False, default='measurements.csv',         help="path to file where results (as CSV) will be save")
+    output.add_argument('--output-file', type=str, required=False, default='measurements.csv',         help="path to file where results (as CSV) will be saved")
     output.add_argument('--dry-run',               required=False, default=False, action='store_true', help="do a dry run, i.e. don't save results to disk")
 
     return parser.parse_args()
@@ -54,7 +53,12 @@ def process(args):
         print("No files to process in '%s' with pattern '%s'." % (args.input_dir,args.input_pattern))
         sys.exit(0)
 
-    get_threshold = eval(f"thresholding.threshold_{args.threshold_method}")
+    if args.threshold_value is not None:
+        print(f"Using fixed threshold value {args.threshold_value}", flush=True)
+        get_threshold = lambda img: args.threshold_value
+    else:
+        print(f"Using '{args.threshold_method}' threshold method", flush=True)
+        get_threshold = eval(f"thresholding.threshold_{args.threshold_method}")
 
     measurements = {}
 
@@ -72,20 +76,12 @@ def process(args):
 
     df = pd.DataFrame(measurements, index=[f.name for f in file_list])
     if not args.dry_run:
-        print(f"{'overwriting' if Path(args.output_file).exists() else 'saving'} '{args.output_file}'")
+        print(f"{'Overwriting' if Path(args.output_file).exists() else 'Saving'} '{args.output_file}'")
         df.to_csv(str(args.output_file))
-
-    # display(df)
-
 
 
 if __name__ == '__main__':
-    if not ('__file__' in locals() or '__file__' in globals()):
-        print('running interactively, exiting.')
-        sys.exit(0)
-
     # parse arguments
     args = parse_args()
-
     # execute workflow
     process(args)
